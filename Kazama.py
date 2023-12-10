a338d558c864bceef911887bf892adfa4c3a9772
PA-AIT <153344795+PA-AIT@users.noreply.github.com>
2023-12-11 00:12:35 +0530

    Update Kazama.py

--- Kazama.py
+++ Kazama.py
@@ -5,7 +5,7 @@ import nltk
 from io import BytesIO
 import fitz  # PyMuPDF
 import streamlit as st
-from summarizer import BertSummarizer  # Correct import
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Download NLTK resources
 nltk.download('punkt')
@@ -26,78 +26,49 @@ def extract_text_from_pdf(pdf_bytes):
         text += pdf_document[page_num].get_text()
     return text
 
-# Function to summarize text using BERT-based summarizer
-def summarize_text_bert(text):
-    summarizer = BertSummarizer()  # Correct instantiation
-    summary = summarizer(text)
+# Function to summarize text using T5 model
+def summarize_text_t5(text):
+    model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    tokenizer = T5Tokenizer.from_pretrained('t5-small')
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
+# ... (Previous code remains the same)
 
-# Specify the date for filtering
-selected_date = st.text_input("Enter the date (YYYY-MM-DD) to filter emails")
-
-# Convert date format for IMAP search
-try:
-    imap_date_format = pd.to_datetime(selected_date).strftime("%d-%b-%Y").upper()
-except Exception as e:
-    st.error(f"Error converting date format: {str(e)}")
-    st.stop()
-
+# IMAP client setup for fetching emails
 if st.button("Fetch and Display PDF Summaries"):
     try:
-        # URL for IMAP connection
-        imap_url = 'imap.gmail.com'
+        # IMAP connection setup
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(user, password)
+        mail.select("inbox")
 
-        # Connection with GMAIL using SSL
-        with imaplib.IMAP4_SSL(imap_url) as my_mail:
-            # Log in using user and password
-            my_mail.login(user, password)
+        # Search for emails based on the specified date
+        result, data = mail.search(None, f'(SINCE {selected_date})')
 
-            # Select the Inbox to fetch messages
-            my_mail.select('inbox')
+        # Iterate through the fetched email IDs
+        for num in data[0].split():
+            result, data = mail.fetch(num, "(RFC822)")
+            raw_email = data[0][1]
+            msg = email.message_from_bytes(raw_email)
 
-            # Define the key and value for email search
-            key = 'SINCE'
-            value = imap_date_format  # Use the user-specified date to search
-            _, data = my_mail.search(None, key, value)
+            # Process the email content
+            email_date = msg["Date"]
+            email_subject = msg["Subject"]
 
-            mail_id_list = data[0].split()
+            for part in msg.walk():
+                if part.get_content_type() == "application/pdf":
+                    # Extract text from PDF using PyMuPDF
+                    pdf_bytes = part.get_payload(decode=True)
+                    pdf_text = extract_text_from_pdf(pdf_bytes)
 
-            info_list = []
+                    # Summarize the PDF content using T5-based summarizer
+                    summary = summarize_text_t5(pdf_text)
 
-            # Iterate through messages
-            for num in mail_id_list:
-                typ, data = my_mail.fetch(num, '(RFC822)')
-                msg = email.message_from_bytes(data[0][1])
-
-                for part in msg.walk():
-                    if part.get_content_type() == 'application/pdf':
-                        # Extract email date
-                        email_date = msg["Date"]
-
-                        # Extract text from PDF using PyMuPDF
-                        pdf_bytes = part.get_payload(decode=True)
-                        pdf_text = extract_text_from_pdf(pdf_bytes)
-
-                        # Summarize the PDF content using BERT-based summarizer
-                        summary = summarize_text_bert(pdf_text)
-
-                        info = {"Summarized Content": summary, "Received Date": email_date}
-                        info_list.append(info)
-
-            # Display the summarized content
-            for info in info_list:
-                st.subheader(f"Received Date: {info['Received Date']}")
-                st.write(info["Summarized Content"])
-
-            # Download button
-            if st.button("Download Summaries as Text File"):
-                summary_text = "\n\n".join(f"Received Date: {info['Received Date']}\n{info['Summarized Content']}" for info in info_list)
-                st.download_button(
-                    label="Download Summaries",
-                    data=summary_text,
-                    key="download_summaries_txt",
-                    file_name="summaries.txt",
-                )
+                    # Display the summarized content
+                    st.subheader(f"Received Date: {email_date}, Subject: {email_subject}")
+                    st.write(summary)
 
     except Exception as e:
         st.error(f"An error occurred during IMAP connection: {str(e)}")
